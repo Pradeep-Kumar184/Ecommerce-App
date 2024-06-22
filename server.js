0ebde3aa6e344e31afac7b530e33ce5b6cbba21a
Pradeep-Kumar <142787893+Pradeep-Kumar184@users.noreply.github.com>
2024-06-22 12:22:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 // port
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`.bgCyan.yellow);
 });
